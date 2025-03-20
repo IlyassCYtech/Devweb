fc97c772db4a1f4b9fa204b7e2232dd1018971a0
IlyassCYtech <130382885+IlyassCYtech@users.noreply.github.com>
2025-03-20 18:38:43 +0100

    Delete includes/header.php

--- includes/header.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html lang='fr'>
-<head>
-    <meta charset='UTF-8'>
-    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-    <title>Mon Projet</title>
-    <link href='/assets/css/bootstrap.min.css' rel='stylesheet'>
-</head>
-<body>
