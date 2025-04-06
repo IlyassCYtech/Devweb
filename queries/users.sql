19ad7cc5bd6b29f67b742385a9d54c752adee9a6
KiraAme <77437133+KiraAme@users.noreply.github.com>
2025-04-06 21:51:34 +0200

    Add files via upload

--- queries/users.sql
+++ queries/users.sql
@@ -9,6 +9,7 @@ DROP TABLE IF EXISTS NivUtilisateur;
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS TypeObjet;
 DROP TABLE IF EXISTS email_confirmations;
+DROP TABLE IF EXISTS DemandesTypeObjet;
 
 -- Création de la table des utilisateurs
 CREATE TABLE users (
@@ -108,6 +109,16 @@ CREATE TABLE UserHistory (
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
+-- Création de la table des demandes de type d'objet
+CREATE TABLE DemandesTypeObjet (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    type_objet VARCHAR(255) NOT NULL,
+    date_demande DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    statut ENUM('En attente', 'Approuvé', 'Rejeté') DEFAULT 'En attente',
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
 INSERT INTO TypeObjet (Nom) VALUES 
 ('Vélo'),
 ('Trottinette'),
@@ -122,6 +133,17 @@ INSERT INTO users (username, password, nom, prenom, date_naissance, age, sexe, t
 VALUES 
 ('janedoe', '123456', 'Doe', 'Jane', '1995-08-20', 28, 'Femme', 'développeur', 'jane.doe@example.com', 'Intermédiaire', 100, 1, 'default.jpg',000001,1,1);
 
+-- Insertion d'un objet connecté avec un utilisateur spécifique
+INSERT INTO ObjetConnecte (Nom, Type, Description, Marque, Etat, Connectivite, EnergieUtilisee, Luminosite, EtatLuminaire, LocalisationGPS, Vitesse, EtatBatterie, UtilisateurID)
+VALUES ('Lampe intelligente', 'Éclairage', 'Lampe connectée avec réglage de lintensité', 'Philips', 'Actif', 'Wi-Fi', 'Électricité', 75.5, 'Allumé', '37.7749,-122.4194', NULL, 95, 1); -- UtilisateurID = 1
+
+-- Insertion d'un autre objet connecté sans utilisateur (UtilisateurID = NULL)
+INSERT INTO ObjetConnecte (Nom, Type, Description, Marque, Etat, Connectivite, EnergieUtilisee, Luminosite, EtatLuminaire, LocalisationGPS, Vitesse, EtatBatterie, UtilisateurID)
+VALUES ('Caméra de sécurité', 'Surveillance', 'Caméra HD avec vision nocturne', 'Arlo', 'Actif', 'Wi-Fi', 'Batterie', NULL, NULL, '37.7749,-122.4194', NULL, 80, NULL); -- Pas d'utilisateur (UtilisateurID = NULL)
+
+-- Insertion d'un autre objet connecté avec un autre utilisateur
+INSERT INTO ObjetConnecte (Nom, Type, Description, Marque, Etat, Connectivite, EnergieUtilisee, Luminosite, EtatLuminaire, LocalisationGPS, Vitesse, EtatBatterie, UtilisateurID)
+VALUES ('Thermostat intelligent', 'Chauffage', 'Thermostat connecté réglable à distance', 'Nest', 'Actif', 'Wi-Fi', 'Électricité', NULL, NULL, '37.7749,-122.4194', NULL, 100, 2); -- UtilisateurID = 2
 
 -- Insertion de trois vélos à San Francisco
 INSERT INTO ObjetConnecte (Nom, Type, Description, Marque, Etat, Connectivite, EnergieUtilisee, DateAjout, Vitesse, EtatBatterie, LocalisationGPS)
